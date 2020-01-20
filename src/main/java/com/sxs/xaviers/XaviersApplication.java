39472be6bb86d7cf70b1f786fdafa1dca070f1e1
Deepak Kumar <deepak.k@tavant.com>
2020-01-20 15:07:42 +0530

    added initial stuffs

---
+++ src/main/java/com/sxs/xaviers/XaviersApplication.java
@@ -0,0 +1,13 @@
+package com.sxs.xaviers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XaviersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XaviersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sxs/xaviers/XaviersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sxs.xaviers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XaviersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
